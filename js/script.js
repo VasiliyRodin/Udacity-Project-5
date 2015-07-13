bb6b1e04a6c3953885fce06081daace6bdbdd245
Vasiliy Rodin <vrodin@Vasiliys-MacBook-Pro.local>
2015-07-13 11:54:24 -0700

    added wiki

--- js/script.js
+++ js/script.js
@@ -16,15 +16,47 @@ function loadData() {
     var cityStr = $('#city').val();
     var address = streetStr + ', ' + cityStr;
 
+
     var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     $body.append('<img class="bgimg" src="' + streetViewURL + '">');
 
     // YOUR CODE GOES HERE!
     // getting NYT articles about the location
 
-    $.getJSON(URL, function (data) {
-        console.log(data);
+    var apiKey = 'ac6f2aef5c84e42d6dd6b4e0f3dc8d4d:10:72368550';
+    var nyTimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=' + apiKey;
+
+    $.getJSON(nyTimesURL, function (data) {
+        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+        articles = data.response.docs;
+        for (var i = 0; i <articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+ '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '<p>' + '</li>');
+        };
+    }).error(function(){
+        $nytHeaderElem.text('ERRRRRROOOOORRRR');
     });
+
+    var wikiRequestTimeOut = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");}, 8000
+    );
+
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallBack';
+    //Sends the request to WIkipedia 
+    $.ajax({
+        url : wikiUrl,
+        dataType : "jsonp",
+        success: function ( response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="'+ url + '">' + articleStr + '<a></li?>');
+            };
+        }
+    });
+
 
     return false;
 };
