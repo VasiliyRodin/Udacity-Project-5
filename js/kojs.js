4f44e1daa8d7406181739221c4a9543512a12b91
Vasiliy Rodin <vrodin@Vasiliys-MacBook-Pro.local>
2015-07-21 16:53:22 -0700

    Adding Bootstrap

--- js/kojs.js
+++ js/kojs.js
@@ -1,12 +1,6 @@
 // Here's my data model
 var ViewModel = function() {
-    this.city = ko.observable("Dublin");
-    this.state = ko.observable("Ca");
- 
-    this.address = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return " in " + this.city() + " " + this.state();
-    }, this);
+    
 };
  
 ko.applyBindings(new ViewModel()); 
--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 
 function loadData() {
+    /*
+     * List of Restaurants
+     */
 
-    var $body = $('body');
-    var $wikiElem = $('#wikipedia-links');
-    var $yelpElem = $('#yelp-header');
-    var $yelpArtic = $('#yelp-articles');
-    var $greeting = $('#greeting');
-
-    // clear out old data before new request
-    $wikiElem.text("");
-    $yelpArtic.text("");
-
-
+    var tacoPlaces = [
+        {name: " ", street: " ", city: " ", state: " "},
+        {name: " ", street: " ", city: " ", state: " "},
+        {name: " ", street: " ", city: " ", state: " "},
+        {name: " ", street: " ", city: " ", state: " "},
+        {name: " ", street: " ", city: " ", state: " "},
+        {name: " ", street: " ", city: " ", state: " "},
+    ]
     var stateStr = $('#state').val();
     var cityStr = $('#city').val();
     var yelpAddress = stateStr + ", " + cityStr;
@@ -24,7 +24,7 @@ function loadData() {
     function nonce_generate() {
         return (Math.floor(Math.random() * 1e12).toString());
     }
-    // Search for the top 10 highest rated tacos
+    // Search for the top rated tacos
     var yelp_url = "http://api.yelp.com/v2/search";//"&sort2&limit=10";
 
     var parameters = {
@@ -35,34 +35,27 @@ function loadData() {
         oauth_signature_method: 'HMAC-SHA1',
         oauth_version: '1.0',
         callback: 'cb',
-        location : yelpAddress,
-        term : "tacos",
+        location: yelpAddress,
+        term: "tacos",
         limit: "1"
     };
-    
-    var encodedSignature = oauthSignature.generate('GET',yelp_url, parameters,"9Oy6r-k0gjPU7xhX7XKr01rGv-8","7LHXDy-s0D7_CxJNeGY20Pd0HnQ");
+
+    var encodedSignature = oauthSignature.generate('GET', yelp_url, parameters, "9Oy6r-k0gjPU7xhX7XKr01rGv-8", "7LHXDy-s0D7_CxJNeGY20Pd0HnQ");
     parameters.oauth_signature = encodedSignature;
     var settings = {
         url: yelp_url,
         data: parameters,
         cache: true,
         dataType: 'jsonp',
-        success: function(results) {
+        success: function (results) {
             console.log(results);
         },
-        error: function() {
+        error: function () {
             //do stuff
         }
-        
+
     };
     $.ajax(settings);
-
-
-
-
-
-
-
 
     /*
      var streetViewURL = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
