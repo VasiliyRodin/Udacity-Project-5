1ab57d018800214bf95913d9414bcfdc163bd271
Vasiliy Rodin <vrodin@Vasiliys-MacBook-Pro.local>
2015-08-03 13:43:23 -0700

    The map update works !!!

--- js/kojs.js
+++ js/kojs.js
@@ -1,82 +1,122 @@
-var tacoPlaces = [
-    {
-        name: "Tacos Negris",
-        street: "37721 Niles Blvd",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.576258,
-        long: -121.976626
-    },
-    {
-        name: "Tacos La Perla",
-        street: "41080 Trimboli Way",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.531579,
-        long: -121.960496
-    },
-    {
-        name: "Los Cabos",
-        street: "3283 Walnut Ave",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.550253,
-        long: -121.980229
-    },
-    {
-        name: "Casa De Meza",
-        street: "4949 Stevenson Blvd",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.529590,
-        long: -121.982879
-    },
-    {
-        name: "Tacos El Compadre",
-        street: "4149 Peralta Blvd",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.556985,
-        long: -122.008457
-    },
-    {
-        name: " Super Taco",
-        street: "40798 Fremont Blvd",
-        city: "Fremont",
-        state: "CA",
-        lat: 37.534687,
-        long: -121.962361
-    }
-];
-//GoogleMaps API
-function initialize() {
-    var mapOptions = {
-        center: {lat: 37.5483333, lng: -121.9875},
-        zoom: 12
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-            mapOptions);
+function ViewModel() {
+    self = this;
+    self.query = ko.observable("");
+    self.tacoPlaces = [
+        {
+            name: "Tacos Negris",
+            street: "37721 Niles Blvd",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.576258,
+            long: -121.976626,
+            marker: null
+        },
+        {
+            name: "Tacos La Perla",
+            street: "41080 Trimboli Way",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.531579,
+            long: -121.960496,
+            marker: null
+        },
+        {
+            name: "Los Cabos",
+            street: "3283 Walnut Ave",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.550253,
+            long: -121.980229,
+            marker: null
+        },
+        {
+            name: "Casa De Meza",
+            street: "4949 Stevenson Blvd",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.529590,
+            long: -121.982879,
+            marker: null
+        },
+        {
+            name: "Tacos El Compadre",
+            street: "4149 Peralta Blvd",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.556985,
+            long: -122.008457,
+            marker: null
+        },
+        {
+            name: " Super Taco",
+            street: "40798 Fremont Blvd",
+            city: "Fremont",
+            state: "CA",
+            lat: 37.534687,
+            long: -121.962361,
+            marker: null
+        }
+    ];
 
-    for (var i = 0; i < tacoPlaces.length; i++) {
-        var tacoPlace = tacoPlaces[i];
-        var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(tacoPlace.lat, tacoPlace.long),
-            title: tacoPlace.name
+    //GoogleMaps API
+
+    var map;
+    self.tacoPlace = ko.observableArray(self.tacoPlaces);
+    function initialize() {
+        var mapOptions = {
+            center: {lat: 37.5483333, lng: -121.9875},
+            zoom: 12,
+        };
+        map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+
+        self.markerArray = [];
+        for (var i = 0; i < self.tacoPlace().length; i++) {
+            tacoPlace = self.tacoPlace()[i];
+            var marker = new google.maps.Marker({
+                position: new google.maps.LatLng(tacoPlace.lat, tacoPlace.long),
+                title: self.name
+            });
+            self.markerArray.push(marker);
+            tacoPlace.marker = marker;
+        }
+        //shows Marker
+        for (var i = 0; i < self.markerArray.length; i++) {
+            self.markerArray[i].setMap(map);
+        }
+        console.log(self.markerArray)
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
+    self.mapMarker = ko.observableArray(self.markerArray);
+    self.computedTacoPlaces = ko.computed(function () {
+        return ko.utils.arrayFilter(self.tacoPlace(), function (item) {
+            var showItem = item.name.toLowerCase().indexOf(self.query().toLowerCase()) >= 0;
+            if (item.marker) {
+                if (showItem) {
+                    item.marker.setMap(map);
+                } else {
+                    item.marker.setMap(null);
+                }
+            }
+            return showItem;
         });
-        marker.setMap(map);
-    }
-    marker.setMap(map);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-var viewModel = {
-    query: ko.observable('')
-};
-viewModel.tacoPlaces = ko.dependentObservable(function () {
-    var search = this.query().toLowerCase();
-    return ko.utils.arrayFilter(tacoPlaces, function (tacoPlace) {
-        return tacoPlace.name.toLowerCase().indexOf(search) >= 0;
     });
-}, viewModel);
-ko.applyBindings(viewModel);
+
+}
+ko.applyBindings(new ViewModel());
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
